f44a515444a84d200e1351dc2e8b9773f57e9554
Mohamed-Kh-alid <mohamedkhaled25102@gmail.com>
2022-04-06 17:55:43 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ProductsComponent} from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'home' , component : HomeComponent },
+  {path : 'component' , component : ProductsComponent },
+  {path : 'footer' , component : FooterComponent },
+  {path : '' , component : HomeComponent },
+  {path : '**' , component : FooterComponent },
+  
+
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
